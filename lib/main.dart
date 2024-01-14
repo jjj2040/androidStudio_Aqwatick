ff19f25826da4a9ba29979aa78012ef532053cf9
Android4 <jjj2040jjj2040@gmail.com>
2024-01-14 23:26:05 +0300

    Сбросил и добавил5

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 
 
-import 'dart:ffi';
+//import 'dart:ffi';
 
 import 'package:flutter/material.dart';
-import 'package:animated_text_kit/animated_text_kit.dart';
-import 'package:convex_bottom_bar/convex_bottom_bar.dart';
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
-import 'package:untitled111/bottomnavbar.dart';
+//import 'package:animated_text_kit/animated_text_kit.dart';
+//import 'package:convex_bottom_bar/convex_bottom_bar.dart';
+//import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+//import 'package:untitled111/bottomnavbar.dart';
 import 'package:untitled111/password.dart';
-import 'package:page_transition/page_transition.dart';
+//import 'package:page_transition/page_transition.dart';
+//import 'package:mysql1/mysql1.dart';
 
 
 void main() {
